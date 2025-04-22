1d6275084506808c4d8e7a1ccb03b1ed683f5a3d
dk134dk <dk134dk@yandex.com>
2025-04-22 23:01:34 +0800

    Update diy.sh

--- N1-IMM/diy.sh
+++ N1-IMM/diy.sh
@@ -12,21 +12,147 @@ function git_sparse_clone() {
 # Default IP
 # sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 
+# lucky
+rm -rf feeds/packages/net/lucky
+rm -rf feeds/luci/applications/luci-app-lucky
+git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/lucky
 
-# Add packages
-#添加科学上网源
+# msd_lite用23.05自带
+# rm -rf feeds/packages/net/msd_lite
+# rm -rf feeds/luci/applications/luci-app-msd_lite
+# git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
+# git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
+
+# golang版本修复
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
+# mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/luci/applications/luci-app-mosdns
+#git clone -b v5-lua --single-branch --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# 添加passwall
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+
+# nps
+rm -rf feeds/packages/net/nps
+rm -rf feeds/luci/applications/luci-app-nps
+git clone --depth=1 https://github.com/djylb/nps-openwrt package/nps-openwrt
+
+# Fix
+git clone --depth=1 https://github.com/kenzok8/small-package package/small
+
+# timecontrol
+rm -rf feeds/luci/applications/luci-app-timecontrol
+mv package/small/luci-app-timecontrol package/luci-app-timecontrol
+
+# openclash
+rm -rf feeds/luci/applications/luci-app-openclash
+mv package/small/luci-app-openclash package/luci-app-openclash
+
+# fileassistant
+rm -rf feeds/luci/applications/luci-app-fileassistant
+mv package/small/luci-app-fileassistant package/luci-app-fileassistant
+
+# dockerman
+# rm -rf feeds/luci/applications/luci-app-dockerman
+# mv package/small/luci-app-dockerman package/luci-app-dockerman
+
+# serverchan
+# rm -rf feeds/luci/applications/luci-app-serverchan
+# mv package/small/luci-app-wechatpush package/luci-app-wechatpush
+
+# design主题
+rm -rf feeds/luci/themes/luci-theme-design
+mv package/small/luci-theme-design package/luci-theme-design
+mv package/small/luci-app-design-config package/luci-app-design-config
+
+# aria2
+rm -rf feeds/packages/net/aria2
+mv package/small/aria2 feeds/packages/net/aria2
+
+# netdata
+# rm -rf feeds/packages/admin/netdata
+# git clone --depth=1 https://github.com/sirpdboy/openwrt-netdata feeds/packages/admin/netdata
+rm -rf feeds/luci/applications/luci-app-netdata
+mv package/small/luci-app-netdata feeds/luci/applications/luci-app-netdata
+
+# wrtbwmon
+rm -rf feeds/luci/applications/luci-app-wrtbwmon
+mv package/small/wrtbwmon package/wrtbwmon
+mv package/small/luci-app-wrtbwmon package/luci-app-wrtbwmon
+
+# iptvhelper
+mv package/small/iptvhelper package/iptvhelper
+mv package/small/luci-app-iptvhelper package/luci-app-iptvhelper
+
+# cloudflarespeedtest
+# mv package/small/cdnspeedtest package/cdnspeedtest
+mv package/small/luci-app-cloudflarespeedtest package/luci-app-cloudflarespeedtest
+
+# dnsfilter
+mv package/small/luci-app-dnsfilter package/luci-app-dnsfilter
+
+mv package/small/luci-app-wolplus package/luci-app-wolplus
+mv package/small/luci-app-poweroff package/luci-app-poweroff
+mv package/small/luci-app-socat package/luci-app-socat
+
+rm -rf package/small
+
+
+# argon主题
+rm -rf feeds/luci/themes/luci-theme-argon
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone --depth 1 https://github.com/ophub/luci-app-amlogic package/amlogic
-#git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
-git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
-#git clone --depth 1 https://github.com/sbwml/luci-app-alist package/alist
-#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
+
+# 晶晨宝盒
+git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
+
+# ddns-go
+rm -rf feeds/packages/net/ddns-go
+rm -rf feeds/luci/applications/luci-app-ddns-go
+git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
+
+# NetSpeedTest
+git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
+
+# alist
+rm -rf feeds/packages/net/alist
+rm -rf feeds/luci/applications/luci-app-alist
+git clone --depth 1 https://github.com/sbwml/luci-app-alist package/alist
+
+# 启用frp, luci用自带, frp第三方更新版本
+rm -rf feeds/packages/net/frp
+# rm -rf feeds/luci/applications/luci-app-frps
+# rm -rf feeds/luci/applications/luci-app-frpc
+git clone https://github.com/kuoruan/openwrt-frp feeds/packages/net/frp
+# git clone https://github.com/kuoruan/openwrt-frp -b v0.53.2-1 feeds/packages/net/frp
+# git clone https://github.com/user1121114685/frp.git feeds/packages/net/frp
+# rm -rf feeds/luci/applications/luci-app-frps
+# git clone https://github.com/user1121114685/luci-app-frps.git feeds/luci/applications/luci-app-frps
+
+# Set DISTRIB_REVISION
+# sed -i "s/R25.4.145 /Lein Build $(TZ=UTC-8 date "+%Y.%m.%d") For N1 /g" package/lean/default-settings/files/zzz-default-settings
+sed -i 's#mount -t cifs#mount.cifs#g' feeds/luci/applications/luci-app-cifs-mount/root/etc/init.d/cifs
+sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
+
+
+# Default IP
+sed -i 's/192.168.1.1/192.168.6.101/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/N11/g' package/base-files/files/bin/config_generate
 
 
 #删除库中的插件，使用自定义源中的包。
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-wol
 #rm -rf feeds/luci/applications/luci-app-ddns-go
 #rm -rf feeds/packages/net/ddns-go
 #rm -rf feeds/packages/net/alist
